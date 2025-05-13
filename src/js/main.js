c8d65b33a83bdfd51b2358392b489df75aad2b4f
shms0mms <shmsmms01@gmail.com>
2025-05-13 21:39:53 +0800

    feat: nav-buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,18 @@
-// Функция для проверки видимости элемента
-var Visible = function (target) {
-    var targetPosition = {
-            top: window.pageYOffset + target.getBoundingClientRect().top,
-            left: window.pageXOffset + target.getBoundingClientRect().left,
-            right: window.pageXOffset + target.getBoundingClientRect().right,
-            bottom: window.pageYOffset + target.getBoundingClientRect().bottom
-        },
-        windowPosition = {
-            top: window.pageYOffset,
-            left: window.pageXOffset,
-            right: window.pageXOffset + document.documentElement.clientWidth,
-            bottom: window.pageYOffset + document.documentElement.clientHeight
-        };
+const navButtons = document.querySelectorAll("[data-nav-button]");
 
-    if (targetPosition.bottom > windowPosition.top &&
-        targetPosition.top < windowPosition.bottom &&
-        targetPosition.right > windowPosition.left &&
-        targetPosition.left < windowPosition.right) {
-        console.clear();
-        console.log('Вы видите элемент :)');
-    } else {
-        console.clear();
-    }
-};
+navButtons.forEach((navButton) => {
+	navButton.addEventListener("click", () => {
+		const navButtonId = navButton.dataset.navButton;
 
-// Функция для проверки видимости всех элементов
-var checkAllElementsVisibility = function() {
-    var elements = document.querySelectorAll('.container__exp, .projects, .exp, .about__briefly, .career, .experience');
-    elements.forEach(function(element) {
-        Visible(element);
-    });
-};
+		navButtons.forEach((navButton2) => {
+			navButton2.classList.remove("active");
+		});
+		navButton.classList.add("active");
 
-// Запускаем функцию при прокрутке страницы
-window.addEventListener('scroll', checkAllElementsVisibility);
-
-// А также запустим функцию сразу
-checkAllElementsVisibility();
+		const navButtonTarget = document.querySelector(`#${navButtonId}`);
+		navButtonTarget.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	});
+});

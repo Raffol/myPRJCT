c9c9205739a5737c3d2ac265f2ab0d27d919a7e4
Raffol <eremenko97@bk.ru>
2025-05-14 11:52:19 +0800

    feat: links for projects

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,17 @@
 const navButtons = document.querySelectorAll("[data-nav-button]");
 
-navButtons.forEach((navButton) => {
-	navButton.addEventListener("click", () => {
-		const navButtonId = navButton.dataset.navButton;
+navButtons.forEach((navButton) =>{
+   navButton.addEventListener("click", () =>{
+      const navButtonId = navButton.dataset.navButton;
+      navButtons.forEach((navButton2)=> {
+         navButton2.classList.remove("active");
+      });
+      navButton.classList.add("active");
 
-		navButtons.forEach((navButton2) => {
-			navButton2.classList.remove("active");
-		});
-		navButton.classList.add("active");
-
-		const navButtonTarget = document.querySelector(`#${navButtonId}`);
-		navButtonTarget.scrollIntoView({
-			behavior: "smooth",
-			block: "start",
-		});
-	});
+      const navButtonTarget = document.querySelector(`#${navButtonId}`);
+      navButtonTarget.scrollIntoView({
+                behavior:"smooth",
+                block:"start",
+      });
+   });
 });
